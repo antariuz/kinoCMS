e7dc86527cfea7a297bb2ebd009b8c1fd7738c49
Tymur Foshch <antariuz@gmail.com>
2021-09-20 15:39:25 +0300

    Initial commit

---
+++ src/main/java/avadamedia/kinocms/KinocmsApplication.java
@@ -0,0 +1,13 @@
+package avadamedia.kinocms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinocmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinocmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/avadamedia/kinocms/KinocmsApplicationTests.java
@@ -0,0 +1,13 @@
+package avadamedia.kinocms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinocmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
